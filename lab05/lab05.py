2470a26c6f462de35fbf30bb48423732c7241a3a
captainxyw <captainxyw@gmail.com>
2023-10-01 15:36:06 +0800

    re-initialise lab05

--- lab05/lab05.py
+++ lab05/lab05.py
@@ -1,30 +1,140 @@
-def flatten(s):
-    """Returns a flattened version of list s.
+def berry_finder(t):
+    """Returns True if t contains a node with the value 'berry' and 
+    False otherwise.
 
-    >>> flatten([1, 2, 3])     # normal list
-    [1, 2, 3]
-    >>> x = [1, [2, 3], 4]     # deep list
-    >>> flatten(x)
-    [1, 2, 3, 4]
-    >>> x # Ensure x is not mutated
-    [1, [2, 3], 4]
-    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
-    >>> flatten(x)
-    [1, 1, 1, 1, 1, 1]
-    >>> x
-    [[1, [1, 1]], 1, [1, 1]]
+    >>> scrat = tree('berry')
+    >>> berry_finder(scrat)
+    True
+    >>> sproul = tree('roots', [tree('branch1', [tree('leaf'), tree('berry')]), tree('branch2')])
+    >>> berry_finder(sproul)
+    True
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> berry_finder(numbers)
+    False
+    >>> t = tree(1, [tree('berry',[tree('not berry')])])
+    >>> berry_finder(t)
+    True
     """
-    if not s:
-        return []
-    elif type(s[0]) == list:
-        return flatten(s[0] + flatten(s[1:]))
+    "*** YOUR CODE HERE ***"
+
+
+def replace_loki_at_leaf(t, lokis_replacement):
+    """Returns a new tree where every leaf value equal to "loki" has
+    been replaced with lokis_replacement.
+
+    >>> yggdrasil = tree('odin',
+    ...                  [tree('balder',
+    ...                        [tree('loki'),
+    ...                         tree('freya')]),
+    ...                   tree('frigg',
+    ...                        [tree('loki')]),
+    ...                   tree('loki',
+    ...                        [tree('sif'),
+    ...                         tree('loki')]),
+    ...                   tree('loki')])
+    >>> laerad = copy_tree(yggdrasil) # copy yggdrasil for testing purposes
+    >>> print_tree(replace_loki_at_leaf(yggdrasil, 'freya'))
+    odin
+      balder
+        freya
+        freya
+      frigg
+        freya
+      loki
+        sif
+        freya
+      freya
+    >>> laerad == yggdrasil # Make sure original tree is unmodified
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+
+
+# Tree ADT
+
+def tree(label, branches=[]):
+    """Construct a tree with the given label value and a list of branches."""
+    if change_abstraction.changed:
+        for branch in branches:
+            assert is_tree(branch), 'branches must be trees'
+        return {'label': label, 'branches': list(branches)}
     else:
-        return [s[0]] + flatten(s[1:])
+        for branch in branches:
+            assert is_tree(branch), 'branches must be trees'
+        return [label] + list(branches)
+
+def label(tree):
+    """Return the label value of a tree."""
+    if change_abstraction.changed:
+        return tree['label']
+    else:
+        return tree[0]
+
+def branches(tree):
+    """Return the list of branches of the given tree."""
+    if change_abstraction.changed:
+        return tree['branches']
+    else:
+        return tree[1:]
+
+def is_tree(tree):
+    """Returns True if the given tree is a tree, and False otherwise."""
+    if change_abstraction.changed:
+        if type(tree) != dict or len(tree) != 2:
+            return False
+        for branch in branches(tree):
+            if not is_tree(branch):
+                return False
+        return True
+    else:
+        if type(tree) != list or len(tree) < 1:
+            return False
+        for branch in branches(tree):
+            if not is_tree(branch):
+                return False
+        return True
+
+def is_leaf(tree):
+    """Returns True if the given tree is a leaf. (i.e. It has no branches.)"""
+    return not branches(tree)
+
+def print_tree(t, indent=0):
+    """Print a representation of this tree in which each node is
+    indented by two spaces times its depth from the root.
+
+    >>> print_tree(tree(1))
+    1
+    >>> print_tree(tree(1, [tree(2)]))
+    1
+      2
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> print_tree(numbers)
+    1
+      2
+      3
+        4
+        5
+      6
+        7
+    """
+    print('  ' * indent + str(label(t)))
+    for b in branches(t):
+        print_tree(b, indent + 1)
+
+def copy_tree(t):
+    """Returns a copy of t. Only for testing purposes.
+
+    >>> t = tree(5)
+    >>> copy = copy_tree(t)
+    >>> t = tree(6)
+    >>> print_tree(copy)
+    5
+    """
+    return tree(label(t), [copy_tree(b) for b in branches(t)])
 
 
 from math import sqrt
-
-
 def distance(city_a, city_b):
     """
     >>> city_a = make_city('city_a', 0, 1)
@@ -37,7 +147,6 @@ def distance(city_a, city_b):
     5.0
     """
     "*** YOUR CODE HERE ***"
-
 
 def closer_city(lat, lon, city_a, city_b):
     """
@@ -55,7 +164,6 @@ def closer_city(lat, lon, city_a, city_b):
     'Bucharest'
     """
     "*** YOUR CODE HERE ***"
-
 
 def check_city_abstraction():
     """
@@ -82,8 +190,6 @@ def check_city_abstraction():
 
 # Treat all the following code as being behind an abstraction layer,
 # you shouldn't need to look at it.
-
-
 def make_city(name, lat, lon):
     """
     >>> city = make_city('Berkeley', 0, 1)
@@ -95,10 +201,9 @@ def make_city(name, lat, lon):
     1
     """
     if change_abstraction.changed:
-        return {"name": name, "lat": lat, "lon": lon}
+        return {"name" : name, "lat" : lat, "lon" : lon}
     else:
         return [name, lat, lon]
-
 
 def get_name(city):
     """
@@ -111,7 +216,6 @@ def get_name(city):
     else:
         return city[0]
 
-
 def get_lat(city):
     """
     >>> city = make_city('Berkeley', 0, 1)
@@ -122,7 +226,6 @@ def get_lat(city):
         return city["lat"]
     else:
         return city[1]
-
 
 def get_lon(city):
     """
@@ -138,161 +241,41 @@ def get_lon(city):
 ###############
 
 
-def berry_finder(t):
-    """Returns True if t contains a node with the value 'berry' and 
-    False otherwise.
-
-    >>> scrat = tree('berry')
-    >>> berry_finder(scrat)
+def dejavu(t, n):
+    """
+    >>> my_tree = tree(2, [tree(3, [tree(5), tree(7)]), tree(4)])
+    >>> dejavu(my_tree, 12) # 2 -> 3 -> 7
     True
-    >>> sproul = tree('roots', [tree('branch1', [tree('leaf'), tree('berry')]), tree('branch2')])
-    >>> berry_finder(sproul)
-    True
-    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
-    >>> berry_finder(numbers)
+    >>> dejavu(my_tree, 5) # Sums of partial paths like 2 -> 3 don ’t count
     False
-    >>> t = tree(1, [tree('berry',[tree('not berry')])])
-    >>> berry_finder(t)
-    True
     """
     "*** YOUR CODE HERE ***"
 
 
-def sprout_leaves(t, leaves):
-    """Sprout new leaves containing the data in leaves at each leaf in
-    the original tree t and return the resulting tree.
-
-    >>> t1 = tree(1, [tree(2), tree(3)])
-    >>> print_tree(t1)
+def hailstone_tree(n, h):
+    """Generates a tree of hailstone numbers that will reach N, with height H.
+    >>> print_tree(hailstone_tree(1, 0))
     1
-      2
-      3
-    >>> new1 = sprout_leaves(t1, [4, 5])
-    >>> print_tree(new1)
+    >>> print_tree(hailstone_tree(1, 4))
     1
       2
         4
+          8
+            16
+    >>> print_tree(hailstone_tree(8, 3))
+    8
+      16
+        32
+          64
         5
-      3
-        4
-        5
-
-    >>> t2 = tree(1, [tree(2, [tree(3)])])
-    >>> print_tree(t2)
-    1
-      2
-        3
-    >>> new2 = sprout_leaves(t2, [6, 1, 2])
-    >>> print_tree(new2)
-    1
-      2
-        3
-          6
-          1
-          2
+          10
     """
-    "*** YOUR CODE HERE ***"
-
-# Abstraction tests for sprout_leaves and berry_finder
-
-
-def check_abstraction():
-    """
-    There's nothing for you to do for this function, it's just here for the extra doctest
-    >>> change_abstraction(True)
-    >>> scrat = tree('berry')
-    >>> berry_finder(scrat)
-    True
-    >>> sproul = tree('roots', [tree('branch1', [tree('leaf'), tree('berry')]), tree('branch2')])
-    >>> berry_finder(sproul)
-    True
-    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
-    >>> berry_finder(numbers)
-    False
-    >>> t = tree(1, [tree('berry',[tree('not berry')])])
-    >>> berry_finder(t)
-    True
-    >>> t1 = tree(1, [tree(2), tree(3)])
-    >>> print_tree(t1)
-    1
-      2
-      3
-    >>> new1 = sprout_leaves(t1, [4, 5])
-    >>> print_tree(new1)
-    1
-      2
-        4
-        5
-      3
-        4
-        5
-
-    >>> t2 = tree(1, [tree(2, [tree(3)])])
-    >>> print_tree(t2)
-    1
-      2
-        3
-    >>> new2 = sprout_leaves(t2, [6, 1, 2])
-    >>> print_tree(new2)
-    1
-      2
-        3
-          6
-          1
-          2
-    >>> change_abstraction(False)
-    """
-
-
-def preorder(t):
-    """Return a list of the entries in this tree in the order that they
-    would be visited by a preorder traversal (see problem description).
-
-    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
-    >>> preorder(numbers)
-    [1, 2, 3, 4, 5, 6, 7]
-    >>> preorder(tree(2, [tree(4, [tree(6)])]))
-    [2, 4, 6]
-    """
-    "*** YOUR CODE HERE ***"
-
-
-def add_trees(t1, t2):
-    """
-    >>> numbers = tree(1,
-    ...                [tree(2,
-    ...                      [tree(3),
-    ...                       tree(4)]),
-    ...                 tree(5,
-    ...                      [tree(6,
-    ...                            [tree(7)]),
-    ...                       tree(8)])])
-    >>> print_tree(add_trees(numbers, numbers))
-    2
-      4
-        6
-        8
-      10
-        12
-          14
-        16
-    >>> print_tree(add_trees(tree(2), tree(3, [tree(4), tree(5)])))
-    5
-      4
-      5
-    >>> print_tree(add_trees(tree(2, [tree(3)]), tree(2, [tree(3), tree(4)])))
-    4
-      6
-      4
-    >>> print_tree(add_trees(tree(2, [tree(3, [tree(4), tree(5)])]), \
-    tree(2, [tree(3, [tree(4)]), tree(5)])))
-    4
-      6
-        8
-        5
-      5
-    """
-    "*** YOUR CODE HERE ***"
+    if _________________________________:
+        return _________________________________
+    branches = _________________________________
+    if ___________ and ___________ and ___________:
+        branches += _________________________________
+    return tree(n, branches)
 
 
 def change_abstraction(change):
@@ -304,96 +287,5 @@ def change_abstraction(change):
     """
     change_abstraction.changed = change
 
-
 change_abstraction.changed = False
 
-
-# Tree ADT
-
-def tree(label, branches=[]):
-    """Construct a tree with the given label value and a list of branches."""
-    if change_abstraction.changed:
-        for branch in branches:
-            assert is_tree(branch), 'branches must be trees'
-        return {'label': label, 'branches': list(branches)}
-    else:
-        for branch in branches:
-            assert is_tree(branch), 'branches must be trees'
-        return [label] + list(branches)
-
-
-def label(tree):
-    """Return the label value of a tree."""
-    if change_abstraction.changed:
-        return tree['label']
-    else:
-        return tree[0]
-
-
-def branches(tree):
-    """Return the list of branches of the given tree."""
-    if change_abstraction.changed:
-        return tree['branches']
-    else:
-        return tree[1:]
-
-
-def is_tree(tree):
-    """Returns True if the given tree is a tree, and False otherwise."""
-    if change_abstraction.changed:
-        if type(tree) != dict or len(tree) != 2:
-            return False
-        for branch in branches(tree):
-            if not is_tree(branch):
-                return False
-        return True
-    else:
-        if type(tree) != list or len(tree) < 1:
-            return False
-        for branch in branches(tree):
-            if not is_tree(branch):
-                return False
-        return True
-
-
-def is_leaf(tree):
-    """Returns True if the given tree's list of branches is empty, and False
-    otherwise.
-    """
-    return not branches(tree)
-
-
-def print_tree(t, indent=0):
-    """Print a representation of this tree in which each node is
-    indented by two spaces times its depth from the root.
-
-    >>> print_tree(tree(1))
-    1
-    >>> print_tree(tree(1, [tree(2)]))
-    1
-      2
-    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
-    >>> print_tree(numbers)
-    1
-      2
-      3
-        4
-        5
-      6
-        7
-    """
-    print('  ' * indent + str(label(t)))
-    for b in branches(t):
-        print_tree(b, indent + 1)
-
-
-def copy_tree(t):
-    """Returns a copy of t. Only for testing purposes.
-
-    >>> t = tree(5)
-    >>> copy = copy_tree(t)
-    >>> t = tree(6)
-    >>> print_tree(copy)
-    5
-    """
-    return tree(label(t), [copy_tree(b) for b in branches(t)])
